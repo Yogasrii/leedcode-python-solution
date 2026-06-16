59839ee2bc7135929b16cbc01ee15fd3449801e3
yogasrii <yogasriideenathayalan@gmail.com>
2026-06-16 22:25:46 +0530

    Create leetcode.py

---
+++ leetcode.py
@@ -0,0 +1,13 @@
+# Two Sum
+def twoSum(nums, target):
+    hashmap = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num
+
+        if complement in hashmap:
+            return [hashmap[complement], i]
+
+        hashmap[num] = i
+
+    return []
